fd92132857c7707abf38bf777e803ca0d7835723
Calok <carstengamstjensen@gmail.com>
2021-09-24 13:27:04 +0200

    controller fix for edit movie

--- src/main/java/edu/kea/kinoxp/controllers/MainController.java
+++ src/main/java/edu/kea/kinoxp/controllers/MainController.java
@@ -37,11 +37,11 @@ public class MainController {
 
     //SC
 
-    @GetMapping("/movies/{id}")
+    @GetMapping("/movies/{id}/edit")
     public String editMoviePage(Model model, @PathVariable int id){
-        //Movie movie = movieService.fetchMovieByID(id);
+        Movie movie = movieService.getMovie(id);
         //Movie movie = new Movie(1,"Hestemand", 1, 1, 1, "genre", "description", "actors", "moviepost.jpg");
-        //model.addAttribute("movie", movie);
+        model.addAttribute("movie", movie);
         return "edit-movie.html";
     }
 
