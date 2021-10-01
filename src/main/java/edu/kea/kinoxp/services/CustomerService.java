5c6d88a3d6a8d6f2f49a968808c79e4dbd3b1b8e
acmve <acmverden@hotmail.com>
2021-10-01 10:36:01 +0200

    ""

--- src/main/java/edu/kea/kinoxp/services/CustomerService.java
+++ src/main/java/edu/kea/kinoxp/services/CustomerService.java
@@ -1,4 +1,18 @@
 package edu.kea.kinoxp.services;
 
+import edu.kea.kinoxp.models.Customer;
+import edu.kea.kinoxp.models.Movie;
+import edu.kea.kinoxp.repositories.CustomerRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CustomerService {
+
+    @Autowired
+    CustomerRepo customerRepo;
+
+    public void updateCustomer(Customer c){
+        customerRepo.updateCostumer(c);
+    }
 }
