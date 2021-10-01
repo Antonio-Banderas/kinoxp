5cbdd042bb4802512a7f4a02b49340901f0b74cc
Antonio-Banderas <anto8214@stud.kea.dk>
2021-10-01 10:46:15 +0200

    1: pom: added lombok dependcy + its configuration 2: created ScreeningService class 3: small stuff

--- src/main/java/edu/kea/kinoxp/controllers/ScreeningController.java
+++ src/main/java/edu/kea/kinoxp/controllers/ScreeningController.java
@@ -2,6 +2,7 @@ package edu.kea.kinoxp.controllers;
 
 import edu.kea.kinoxp.repositories.ScreeningRepo;
 import edu.kea.kinoxp.services.MovieService;
+import edu.kea.kinoxp.services.ScreeningService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,19 +14,19 @@ public class ScreeningController {
 
     @Autowired
     MovieService movieService;
-    ScreeningRepo screeningRepo;
+    ScreeningService screeningService;
+
 
     @GetMapping("/movies/{id}/screening")
     public String createScreening(@PathVariable int id, Model model){
 
         String today = "mandag"; // this should be a service that finds current date
         model.addAttribute("film",movieService.getMovie(id));
-        model.addAttribute("otherMovies",screeningRepo.fetchAllScreeningsByDate(today));
+        model.addAttribute("otherMovies",screeningService.getAllScreeningsByDate(today));
 
         // get movies from this date
 
         // model.add(dato,
-
 
         return "create-screening.html";
     }
